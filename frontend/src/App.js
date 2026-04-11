87b91f85e86ff5e190295351bf24e518bdd1c54f
DasunD2002 <dasun5967@gmail.com>
2026-04-11 18:43:15 +0530

    fix issus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import './App.css';
-import Splash from './components/Splash/Splash';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import Home from './components/Home/Home';
-import TrendingJobAnalyzer from './components/TrendingJobAnalyzer/TrendingJobAnalyzer';
-import SavedJobs from './components/SavedJobs/SavedJobs';
-import LearningResources from './components/Learning_Resources/learning_resources';
-import UserDashboard from './components/Dashboards/dashboardUser';
-=======
 import ProtectedRoute from './components/Common/ProtectedRoute';
 import Splash from '../src/components/Splash/Splash';
 import Login from '../src/components/Login/Login';
@@ -22,7 +11,6 @@ import ProfessionalCourses from '../src/components/Learning_Resources/profession
 import LearningRoadmap from '../src/components/Learning_Resources/learning_roadmap';
 import UserDashboard from '../src/components/Dashboards/dashboardUser';
 import DashboardAdmin from '../src/components/Dashboards/dashboardAdmin';
->>>>>>> 6cd59e4fd242289f136f0ef75067c692ffc50fbc
 import CvGenerator from '../src/components/CvGenerator/CvGenerator';
 import Economy from '../src/components/Economy/economy';
 import ProgressPage from '../src/components/Progress/progress_page';
@@ -38,14 +26,6 @@ function App() {
           <Route path="/" element={<Splash />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<UserDashboard />} />
-          <Route path="/cv-generator" element={<CvGenerator />} />
-          <Route path="/job-analyzer" element={<TrendingJobAnalyzer />} />
-          <Route path="/saved-jobs" element={<SavedJobs />} />
-          <Route path="/learning-resources" element={<LearningResources />} />
-=======
 
           {/* Protected User Routes */}
           <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
@@ -60,14 +40,13 @@ function App() {
           <Route path="/progress" element={<ProtectedRoute><ProgressPage /></ProtectedRoute>} />
           <Route path="/recommended" element={<ProtectedRoute><RecommendedCourses /></ProtectedRoute>} />
           <Route path="/skill-analyzer" element={<ProtectedRoute><JobSkillAnalyzer /></ProtectedRoute>} />
-                                       
+
           {/* Protected Admin Routes */}
           <Route path="/admin-dashboard" element={
             <ProtectedRoute adminOnly={true}>
               <DashboardAdmin />
             </ProtectedRoute>
           } />
->>>>>>> 6cd59e4fd242289f136f0ef75067c692ffc50fbc
         </Routes>
       </div>
     </Router>
